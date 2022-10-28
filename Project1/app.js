219250c5891e214b1d1eda251d91db222624b4e6
aleshkey <90507705+lesha-oleshkevich@users.noreply.github.com>
2022-10-28 19:50:52 +0300

    slides

---
+++ Project1/app.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide')
+
+for(const slide of slides){
+    slide.addEventListener('click', ()=>{
+        clearAcriveClasses()
+        slide.classList.add('active')
+    })
+}
+
+function clearAcriveClasses(){
+    slides.forEach((slide)=>{
+        slide.classList.remove('active')
+    })
+}
