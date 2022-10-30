95b58ecdff663dade5300e24eaa21d636a1ffa1c
aleshkey <90507705+lesha-oleshkevich@users.noreply.github.com>
2022-10-30 22:31:57 +0300

    update

--- Project1/app.js
+++ Project1/app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activeSlide = 0){
+    const slides = document.querySelectorAll('.slide')
 
-for(const slide of slides){
-    slide.addEventListener('click', ()=>{
-        clearAcriveClasses()
-        slide.classList.add('active')
-    })
+    slides[activeSlide].classList.add('active')
+
+    for(const slide of slides){
+        slide.addEventListener('click', ()=>{
+            clearAcriveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearAcriveClasses(){
+        slides.forEach((slide)=>{
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearAcriveClasses(){
-    slides.forEach((slide)=>{
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin()
